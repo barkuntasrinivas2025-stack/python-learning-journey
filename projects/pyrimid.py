dfddce46cd8b102c49e7e43100cc1cc3ba8c206e
barkuntasrinivas2025-stack <barkuntasrinivas2025@gmail.com>
2026-05-21 14:03:31 +0530

    Conditional + Loops

--- projects/pyrimid.py
+++ projects/pyrimid.py
@@ -1,3 +1,6 @@
 n = int(input("Enter the number of rows: "))
-for i in range(1, n+1):
-    for j in range(i,i+1):print("*")
+for i in range(n):
+    print((n-i-1)*" ",end=" ")
+    for j in range(0, i+1):
+        print("*",end=" ")
+    print(" ")
