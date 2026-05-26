7aa02c9af1617e78e42d3cb765f580d432b2ea76
barkuntasrinivas2025-stack <barkuntasrinivas2025@gmail.com>
2026-05-26 20:36:21 +0530

    sets functionality

---
+++ programing/sets/setMethods.py
@@ -0,0 +1,9 @@
+fruits = {"grapes","apples"}
+print(fruits)
+fruits.add("mango")
+fruits.add("water mellon")
+print(fruits)
+fruits.remove("grapes")
+print(fruits)
+fruits.discard("apples")
+print(fruits)
